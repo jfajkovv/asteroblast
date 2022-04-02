49b3e7189ebcf6414d5f7c5e812d1d6d39c13b19
jfajkovv <jfajkovv@gmail.com>
2022-04-02 16:10:25 +0200

    create program skeleton: import superwires, initialise screen, define main() function

---
+++ asteroblast.py
@@ -0,0 +1,17 @@
+# Include all necessary tools.
+from superwires import games
+
+# Create screen and get access to games instructions subset.
+games.init(
+    screen_width=800,
+    screen_height=600,
+    fps=60
+)
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
