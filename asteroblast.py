0e547507e5d56104e336906feb142ec06eeecdde
jfajkovv <jfajkovv@gmail.com>
2022-04-02 16:42:20 +0200

    create Spacecraft class w/ it's constructor, place it on the screen; set up some handy constants block in Game class, add transparent parameter to the ORBIT_IMG

--- asteroblast.py
+++ asteroblast.py
@@ -9,11 +9,45 @@ games.init(
 )
 
 
+class Spacecraft(games.Sprite):
+    """An actual player."""
+
+    # Load assets.
+    SPACECRAFT_IMG = games.load_image('./assets/graphics/spacecraft.png')
+
+    def __init__(self, x, y):
+        # Appeal to the Sprite constructor in order
+        # to set up the image and call upon coordinates.
+        super(Spacecraft, self).__init__(
+            image=Spacecraft.SPACECRAFT_IMG,
+            x=x,
+            y=y
+        )
+
+
 class Game(object):
     """Gameplay core mechanics."""
 
     # Load assets.
-    ORBIT_IMG = games.load_image("./assets/graphics/orbit.png")
+    ORBIT_IMG = games.load_image(
+        filename="./assets/graphics/orbit.png",
+        transparent=False
+    )
+
+    # Handy constants.
+    SCREEN_WIDTH_CENTER = games.screen.width/2
+    SCREEN_HEIGHT_CENTER = games.screen.height/2
+
+
+    def __init__(self):
+        # Construct spacecraft object
+        # and add it to the screen.
+        self.spacecraft = Spacecraft(
+            x=Game.SCREEN_WIDTH_CENTER,
+            y=Game.SCREEN_HEIGHT_CENTER
+        )
+        games.screen.add(self.spacecraft)
+
 
     # Allow the player to perform an actual gameplay - level by level.
     def play(self):
