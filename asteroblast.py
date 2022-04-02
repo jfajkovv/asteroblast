2ddfb7758bb125a1a04c201ac5272e071811dc17
jfajkovv <jfajkovv@gmail.com>
2022-04-02 16:27:26 +0200

    add comments

--- asteroblast.py
+++ asteroblast.py
@@ -1,7 +1,7 @@
 # Include all necessary tools.
 from superwires import games
 
-# Create screen and get access to games instructions subset.
+# Create window and get access to games instructions subset.
 games.init(
     screen_width=800,
     screen_height=600,
@@ -12,16 +12,23 @@ games.init(
 class Game(object):
     """Gameplay core mechanics."""
 
+    # Load assets.
     ORBIT_IMG = games.load_image("./assets/graphics/orbit.png")
 
+    # Allow the player to perform an actual gameplay - level by level.
     def play(self):
+        # Set up chosen background.
         games.screen.background = Game.ORBIT_IMG
 
+        # Run the actual game - keep the screen running
+        # by evoking the main loop.
         games.screen.mainloop()
 
 
 def main():
+    # Create Game instance.
     asteroblast = Game()
+    # Defend these skies!
     asteroblast.play()
 
 
